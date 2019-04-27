b62947913ec4f1582aab712fe9d67940b30977ea
dogukan demirci <>
2019-04-27 15:08:57 +0300

    first commit

---
+++ swiftfordeeplearning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swiftfordeeplearning
+//
+//  Created by dogukan demirci on 4/25/19.
+//  Copyright © 2019 dogukan demirci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
