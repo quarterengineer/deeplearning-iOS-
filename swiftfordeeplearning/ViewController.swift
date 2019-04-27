218162cd75ba0549f7a57f3cec1b2ea1cf73df07
dogukan demirci <>
2019-04-27 15:15:01 +0300

    viewController

--- swiftfordeeplearning/ViewController.swift
+++ swiftfordeeplearning/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
