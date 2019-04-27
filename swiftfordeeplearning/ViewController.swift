e0fbb9cb4b022860a39d6a6f5f0055472643554d
dogukan demirci <>
2019-04-27 15:23:19 +0300

    last

--- swiftfordeeplearning/ViewController.swift
+++ swiftfordeeplearning/ViewController.swift
@@ -7,13 +7,67 @@
 //
 
 import UIKit
+import CoreML
+import Vision
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
-class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var textView: UITextView!
+    var imageDetails = ""
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self,action: #selector(selectImage))
+        imageView.addGestureRecognizer(gestureRecognizer)
+        
+        
+        
+    }
+    @objc func selectImage()
+    {
+        let pickerController=UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        pickerController.allowsEditing = true
+        present(pickerController,animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        self.imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+        
+        
+    }
+    
+    @IBAction func buttonClicked(_ sender: Any) {
+        predict(image: imageView.image!)
+        textView.text = self.imageDetails
+    }
+    func predict(image:UIImage)
+    {
+        self.imageDetails = ""
+        do
+        {
+            let model = try! VNCoreMLModel(for: fashionCNN().model)
+            let request = VNCoreMLRequest(model: model)
+            {(request,error)in
+                
+                if var results = request.results as? [VNClassificationObservation]
+                {
+                    let x = results[0]
+                    self.imageDetails += (x.identifier + ("  "+(String(x.confidence*100))))
+                    
+                }
+            }
+            request.imageCropAndScaleOption = .centerCrop
+            let handler = VNImageRequestHandler(cgImage: image.cgImage!, options: [:])
+            do
+            {
+                try handler.perform([request])
+            } catch
+            {
+                print(error)
+            }
+        }
     }
 
 
